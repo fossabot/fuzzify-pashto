e3a534e4e826466937d1dc976f62fbe27e0afbda
openpashto <53797277+openpashto@users.noreply.github.com>
2019-08-15 12:54:24 +0300

    website commit

--- website.5c8017a4.js
+++ website.5c8017a4.js
@@ -27,4 +27,4 @@ var e=require("process");module.exports=function(e){var t={};function n(r){if(t[
 },{"./cjs/react-dom.production.min.js":"jF7N"}],"Focm":[function(require,module,exports) {
 "use strict";var e=u(require("./Demo.js")),r=u(require("react")),t=require("react-dom");function u(e){return e&&e.__esModule?e:{default:e}}(0,t.render)(r.default.createElement(e.default,null),document.getElementById("demo"));
 },{"./Demo.js":"5wri","react":"HdMw","react-dom":"X9zx"}]},{},["Focm"], null)
-//# sourceMappingURL=fuzzify-pashto/website.5c8017a4.js.map
+//# sourceMappingURL=https://www.openpashto.com/fuzzify-pashto/website.5c8017a4.js.map
