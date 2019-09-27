7df9af2b98305eee5556e9bdd24a57fb078d9265
openpashto <53797277+openpashto@users.noreply.github.com>
2019-09-27 19:43:52 -0400

    dist folder ignored

--- dist/types/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IFuzzifyOptions {
-    matchStart?: "word" | "string" | "anywhere";
-    script?: "Pashto" | "Latin";
-    matchWholeWordOnly?: boolean;
-    allowSpacesInWords?: boolean;
-    returnWholeWord?: boolean;
-    es2018?: boolean;
-    ignoreDiacritics?: boolean;
-}
